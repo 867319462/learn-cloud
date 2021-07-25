3018386f3085b241bfd499ebe6bafbd183f2836b
wangxl <sdwxl1995@163.com>
2021-07-25 22:42:01 +0800

    Cluster eurekas

--- cloud-eureka-server7002/src/main/java/com/cloud/EurekaApplication7002.java
+++ cloud-eureka-server7002/src/main/java/com/cloud/EurekaApplication7002.java
@@ -5,17 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * eureka 7001 启动类
- *
+ * eureka 7002 启动类
  * @author wangxl
- * @date 2021/7/25 21:43
+ * @date 2021/7/25 22:36
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication7001 {
+public class EurekaApplication7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication7001.class, args);
+        SpringApplication.run(EurekaApplication7002.class, args);
     }
 
 }
